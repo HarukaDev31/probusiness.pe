4c24d82e046cbd1b8dfc2306cc433cf8fee84ae0
Haruka31Dev <u202412824@upc.edu.pe>
2024-06-03 14:05:55 -0500

    add assets

---
+++ assets/global/app.js
@@ -0,0 +1,476 @@
+/*
+document.querySelector('.bx-portada').addEventListener('mouseover', function () {
+  this.classList.add('hovered');
+});
+*/
+
+
+function checkEmail(email) {
+	var caract = new RegExp(/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/);
+	if (caract.test(email) == false) {
+		$('#correo').closest('.form-group').find('.help-block').html('Email inválido');
+		$('#correo').closest('.form-group').removeClass('text-success').addClass('text-danger');
+		$('#correo').closest('.form-group').find('.help-block').removeClass('text-success');
+		return false;
+	} else {
+		$('#correo').closest('.form-group').find('.help-block').html('Email válido');
+		$('#correo').closest('.form-group').addClass('text-success').removeClass('text-danger');
+		$('#correo').closest('.form-group').find('.help-block').removeClass('text-danger');
+		return true;
+	}
+}
+
+function checkEmailTienda(email) {
+	var caract = new RegExp(/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/);
+	if (caract.test(email) == false) {
+		$('#txt-email').closest('.form-group').find('.help-block').html('Email inválido');
+		$('#txt-email').closest('.form-group').removeClass('text-success').addClass('text-danger');
+		$('#txt-email').closest('.form-group').find('.help-block').removeClass('text-success');
+		return false;
+	} else {
+		$('#txt-email').closest('.form-group').find('.help-block').html('Email válido');
+		$('#txt-email').closest('.form-group').addClass('text-success').removeClass('text-danger');
+		$('#txt-email').closest('.form-group').find('.help-block').removeClass('text-danger');
+		return true;
+	}
+}
+
+$( '#terminos' ).on( 'click', function() {
+    if( $(this).is(':checked') ){
+        // Hacer algo si el checkbox ha sido seleccionado
+        $('#btnEnvio').attr('disabled',false);
+        $('#btnEnvio').removeClass('bg-secondary');
+        $('#btnEnvio').removeClass('text-white');
+        $('#btnEnvio').addClass('fondo-verde');
+        $('#btnEnvio').addClass('color-azul');
+    } else {
+        // Hacer algo si el checkbox ha sido deseleccionado
+        $('#btnEnvio').attr('disabled',true);
+    }
+});
+
+$(document).on('click','.open-menu',function(){
+  
+    $('.box-menu-all').addClass('active');
+})
+
+$(document).on('click','.close-menu',function(){
+    $('.box-menu-all').removeClass('active');
+})
+
+$(document).on('click','.item-menu-unique',function(){
+    $('.box-menu-all').removeClass('active');
+})
+
+
+
+
+$(document).ready(function(){
+  $('.collaborator').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+   
+    dots: false,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 564,
+        settings: {
+          centerMode: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          
+        }
+      }
+    ]
+  });
+
+  $('.custom-prev-button').click(function(){
+      $('.collaborator').slick('slickPrev');
+  });
+
+  $('.custom-next-button').click(function(){
+      $('.collaborator').slick('slickNext');
+  });
+
+   function updateCustomDots(slick, currentIndex) {
+        $('.custom-dot').removeClass('active'); // Elimina la clase "active" de todos los dots
+        $('.custom-dot:eq(' + currentIndex + ')').addClass('active'); // Agrega la clase "active" al dot actual
+    }
+
+    // Inicializa los dots personalizados
+    updateCustomDots($('.collaborator'), 0);
+
+    // Configura los dots personalizados para cambiar de diapositiva al hacer clic
+    $('.custom-dot').click(function() {
+        var dotIndex = $(this).index();
+        $('.collaborator').slick('slickGoTo', dotIndex);
+    });
+
+    // Configura un evento de Slick Slider para actualizar los dots personalizados
+    $('.collaborator').on('afterChange', function(event, slick, currentSlide) {
+        updateCustomDots(slick, currentSlide);
+    });
+
+
+
+
+
+    // shorts 
+
+
+
+    $('.shorts').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+    infinite:true,
+    autoplaySpeed: 5000,
+    dots: false,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 821,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 560,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          
+        }
+      }
+    ]
+  });
+
+  $('.custom-prev-button-short').click(function(){
+      $('.shorts').slick('slickPrev');
+  });
+
+  $('.custom-next-button-short').click(function(){
+      $('.shorts').slick('slickNext');
+  });
+
+  $('.beneficios').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+   
+    dots: false,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          
+        }
+      }
+    ]
+  });
+
+  $('.custom-prev-button-short').click(function(){
+      $('.beneficios').slick('slickPrev');
+  });
+
+  $('.custom-next-button-short').click(function(){
+      $('.beneficios').slick('slickNext');
+  });
+
+
+
+  
+    $('.services').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots: false,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 821,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          centerMode: false,
+           initialSlide: 0,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          
+        }
+      }
+    ]
+  });
+
+  $('.custom-prev-button-services').click(function(){
+      $('.services').slick('slickPrev');
+  });
+
+  $('.custom-next-button-services').click(function(){
+      $('.services').slick('slickNext');
+  });
+
+
+
+   $('.rutacarga').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots: false,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 821,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          centerMode: false,
+           initialSlide: 0,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          
+        }
+      }
+    ]
+  });
+
+   $('.custom-prev-button-services').click(function(){
+      $('.rutacarga').slick('slickPrev');
+  });
+
+  $('.custom-next-button-services').click(function(){
+      $('.rutacarga').slick('slickNext');
+  });
+
+
+
+   $('.satisfechos').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 2.5
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          centerMode: false,
+           initialSlide: 0,
+          slidesToShow: 1.5,
+          slidesToScroll: 1,
+          
+        }
+      }
+    ]
+  });
+
+
+  
+
+
+   
+
+
+
+
+  $('.marcas').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    center:false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: false,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          centerMode: false,
+           initialSlide: 0,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          
+        }
+      }
+    ]
+  });
+
+
+
+
+
+  $('.tiendasbox').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    center:true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots: false,
+    centerMode: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 890,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+  $('.custom-prev-button-tienda').click(function(){
+      $('.tiendasbox').slick('slickPrev');
+  });
+
+  $('.custom-next-button-tienda').click(function(){
+      $('.tiendasbox').slick('slickNext');
+  });
+
+   function updateCustomDotsTienda(slick, currentIndex) {
+        $('.custom-dot-tienda').removeClass('active'); // Elimina la clase "active" de todos los dots
+        $('.custom-dot-tienda:eq(' + currentIndex + ')').addClass('active'); // Agrega la clase "active" al dot actual
+    }
+
+    // Inicializa los dots personalizados
+    updateCustomDotsTienda($('.tiendasbox'), 0);
+
+    // Configura los dots personalizados para cambiar de diapositiva al hacer clic
+    $('.custom-dot-tienda').click(function() {
+        var dotIndex = $(this).index();
+        $('.tiendasbox').slick('slickGoTo', dotIndex);
+    });
+
+    // Configura un evento de Slick Slider para actualizar los dots personalizados
+    $('.tiendasbox').on('afterChange', function(event, slick, currentSlide) {
+        updateCustomDotsTienda(slick, currentSlide);
+    });
+
+})
+
+function playVideoAgenteViajes(id) {
+  var iframe = document.getElementById(id);
+  iframe.src += "?autoplay=1"; // Esto inicia la reproducción
+
+  $('.' + id).addClass('d-none');
+  $('.video-agente_viajes').removeClass('position-absolute');
+  $('.video-agagente_viajesente_viajes').removeClass('d-none');
+}
+
+function playVideoCurso(id) {
+  var iframe = document.getElementById(id);
+  iframe.src += "?autoplay=1"; // Esto inicia la reproducción
+
+  $('.' + id).addClass('d-none');
+  $('.video-curso').removeClass('position-absolute');
+  $('.video-curso').removeClass('d-none');
+}
+
+function playVideoCargaConsolidada(id) {
+  var iframe = document.getElementById(id);
+  iframe.src += "?autoplay=1"; // Esto inicia la reproducción
+
+  $('.' + id).addClass('d-none');
+  $('.video-carga_consolidada').removeClass('position-absolute');
+  $('.video-carga_consolidada').removeClass('d-none');
+}
+
+function playVideoAgenteCompra(id) {
+  var iframe = document.getElementById(id);
+  iframe.src += "?autoplay=1"; // Esto inicia la reproducción
+
+  $('.' + id).addClass('d-none');
+  $('.video-agente_compra').removeClass('position-absolute');
+  $('.video-agente_compra').removeClass('d-none');
+}
+function playVideoAgenteViajes(id) {
+  var iframe = document.getElementById(id);
+  iframe.src += "?autoplay=1"; // Esto inicia la reproducción
+  
+  $('.' + id).addClass('d-none');
+  $('.video-agente_viajes').removeClass('position-absolute');
+  $('.video-agente_viajes').removeClass('d-none');
+}
+
+function playVideo(id) {
+    var iframe = document.getElementById(id);
+    iframe.src += "&autoplay=1"; // Esto inicia la reproducción
+
+    $('.'+id).addClass('d-none');
+    $('.view-iframe').removeClass('position-absolute');
+}
+
+
+function playVideo2(id) {
+    var iframe = document.getElementById(id);
+    iframe.src += "&autoplay=1"; // Esto inicia la reproducción
+
+    $('.'+id).addClass('d-none');
+    $('.view-iframe2').removeClass('position-absolute');
+}
---
+++ assets/js/main.js
@@ -0,0 +1,327 @@
+$(function () {
+	$(".div-alert").hide();
+	$(".div-pago_curso").hide();
+
+	// Validate input
+	$(".input-number").on("input", function () {
+		this.value = this.value.replace(/[^0-9]/g, "");
+	});
+
+	$(".input-letter").on("input", function () {
+		this.value = this.value.replace(/[^a-zA-Z ]/g, "");
+	});
+
+	$("#email").blur(function () {
+		checkEmail($(this).val());
+	});
+
+	// Get the form.
+	var form = $("#contact-form");
+	// Get the messages div.
+	var formMessages = $(".alert-message");
+
+	// Set up an event listener for the contact form.
+	$("#btn-contact").click(function (e) {
+		// Stop the browser from submitting the form.
+		e.preventDefault();
+
+		var nombres = $("#firstname"),
+			lastname = $("#lastname"),
+			celular = $("#celular"),
+			email = $("#email");
+			emailForm = $("#emailform");
+		console.log(nombres.val().length);
+		console.log(lastname.val().length);
+		console.log(celular.val().length);
+		console.log(emailForm);
+		$(".help-block").empty();
+		if (nombres.val().length == 0) {
+			nombres.focus();
+			nombres
+				.closest(".form-group")
+				.find(".help-block")
+				.html("Ingresar nombre");
+			nombres
+				.closest(".form-group")
+				.removeClass("text-success")
+				.addClass("text-danger");
+		}  if (lastname.val().length == 0) {
+			lastname.focus();
+			lastname
+				.closest(".form-group")
+				.find(".help-block")
+				.html("Ingresar apellido");
+			lastname
+				.closest(".form-group")
+				.removeClass("text-success")
+				.addClass("text-danger");
+		}  if (celular.val().length === 0) {
+			celular.focus();
+			celular
+				.closest(".form-group")
+				.find(".help-block")
+				.html("Ingresar celular");
+			celular
+				.closest(".form-group")
+				.removeClass("text-success")
+				.addClass("text-danger");
+		}  if (celular.val().length < 9) {
+			celular.focus();
+			celular
+				.closest(".form-group")
+				.find(".help-block")
+				.html("Ingresar 9 dígitos");
+			celular
+				.closest(".form-group")
+				.removeClass("text-success")
+				.addClass("text-danger");
+		
+		}if (email.val().length === 0) {
+			email.focus();
+			email.closest(".form-group").find(".help-block").html("Ingresar email");
+			email
+				.closest(".form-group")
+				.removeClass("text-success")
+				.addClass("text-danger");
+		} 
+		 if (!checkEmail(email.val())) {
+			email.focus();
+			email.closest(".form-group").find(".help-block").html("Email inválido");
+			email
+				.closest(".form-group")
+				.addClass("text-success")
+				.removeClass("text-danger");
+		} else {
+
+			$(".help-block").empty();
+
+			$("#btn-contact").text("");
+			$("#btn-contact").attr("disabled", true);
+			$("#btn-contact").append(
+				'Enviando <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>'
+			);
+
+			// Serialize the form data.
+			var formData = $(form).serialize();
+			
+			// Submit the form using AJAX.
+			$.ajax({
+				type: "POST",
+				dataType: "json",
+				url: base_url + "sendwhatsapp",
+				data: formData,
+			})
+				.done(function (response) {
+					$(".div-alert").show();
+					// Make sure that the formMessages div has the 'success' class.
+					$(formMessages).removeClass("alert-danger");
+					$(formMessages).addClass("alert-primary");
+
+					$("#btn-contact").text("");
+					$("#btn-contact").attr("disabled", false);
+					$("#btn-contact").append("Enviar mensaje");
+
+					$(formMessages).html(response.message);
+
+					// Clear the form.
+					if (response.status == "success") {
+						//$('#contact-form input, #contact-form email, #contact-form tel,#contact-form textarea').val('');
+
+						url = "https://wa.me/" + $("#web_whatsapp").val() + "?text=";
+						url += response.message_whastapp;
+
+						url = encodeURI(url);
+						window.open(url, "_blank");
+					}
+				})
+				.fail(function (data) {
+					$(".div-alert").show();
+					// Make sure that the formMessages div has the 'error' class.
+					$(formMessages).removeClass("alert-primary");
+					$(formMessages).addClass("alert-danger");
+
+					$("#btn-contact").text("");
+					$("#btn-contact").attr("disabled", false);
+					$("#btn-contact").append("Enviar mensaje");
+
+					// Set the message text.
+					if (data.responseText !== "") {
+						$(formMessages).text(data.responseText);
+					} else {
+						$(formMessages).html("Oops! ocurrió un problema.");
+					}
+				});
+		}
+	});
+
+	// Set up an event listener for the contact form.
+	$("#btn-contact_email").click(function (e) {
+		// Stop the browser from submitting the form.
+		e.preventDefault();
+
+		var nombres = $("#firstname"),
+			lastname = $("#lastname"),
+			celular = $("#celular"),
+			email = $("#email");
+
+		$(".help-block").empty();
+		if (nombres.val().length == 0) {
+			nombres.focus();
+			nombres
+				.closest(".form-group")
+				.find(".help-block")
+				.html("Ingresar nombre");
+			nombres
+				.closest(".form-group")
+				.removeClass("text-success")
+				.addClass("text-danger");
+		}
+		if (lastname.val().length == 0) {
+			lastname.focus();
+			lastname
+				.closest(".form-group")
+				.find(".help-block")
+				.html("Ingresar apellido");
+			lastname
+				.closest(".form-group")
+				.removeClass("text-success")
+				.addClass("text-danger");
+		}
+		if (celular.val().length === 0) {
+			celular.focus();
+			celular
+				.closest(".form-group")
+				.find(".help-block")
+				.html("Ingresar celular");
+			celular
+				.closest(".form-group")
+				.removeClass("text-success")
+				.addClass("text-danger");
+		}
+		if (celular.val().length < 9) {
+			celular.focus();
+			celular
+				.closest(".form-group")
+				.find(".help-block")
+				.html("Ingresar 9 dígitos");
+			celular
+				.closest(".form-group")
+				.removeClass("text-success")
+				.addClass("text-danger");
+		}
+		if (email.val().length === 0) {
+			email.focus();
+			email.closest(".form-group").find(".help-block").html("Ingresar email");
+			email
+				.closest(".form-group")
+				.removeClass("text-success")
+				.addClass("text-danger");
+		}
+		if (!checkEmail(email.val())) {
+			email.focus();
+			email.closest(".form-group").find(".help-block").html("Email inválido");
+			email
+				.closest(".form-group")
+				.addClass("text-success")
+				.removeClass("text-danger");
+		} else {
+			$(".help-block").empty();
+
+			$("#btn-contact_email").text("");
+			$("#btn-contact_email").attr("disabled", true);
+			$("#btn-contact_email").append(
+				'Enviando <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>'
+			);
+
+			// Serialize the form data.
+			var formData = $(form).serialize();
+
+			// Submit the form using AJAX.
+			$.ajax({
+				type: "POST",
+				dataType: "json",
+				url: base_url + "sendemail",
+				data: formData,
+			})
+				.done(function (response) {
+					$(".div-alert").show();
+					// Make sure that the formMessages div has the 'success' class.
+					$(formMessages).removeClass("alert-danger");
+					$(formMessages).addClass("alert-primary");
+
+					$("#btn-contact_email").text("");
+					$("#btn-contact_email").attr("disabled", false);
+					$("#btn-contact_email").append("Enviar mensaje");
+
+					$(formMessages).html(response.message);
+				})
+				.fail(function (data) {
+					$(".div-alert").show();
+					// Make sure that the formMessages div has the 'error' class.
+					$(formMessages).removeClass("alert-primary");
+					$(formMessages).addClass("alert-danger");
+
+					$("#btn-contact_email").text("");
+					$("#btn-contact_email").attr("disabled", false);
+					$("#btn-contact_email").append("Enviar mensaje");
+
+					// Set the message text.
+					if (data.responseText !== "") {
+						$(formMessages).text(data.responseText);
+					} else {
+						$(formMessages).html("Oops! ocurrió un problema.");
+					}
+				});
+		}
+	});
+});
+
+function checkEmail(email) {
+	var caract = new RegExp(
+		/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/
+	);
+	if (caract.test(email) == false) {
+		$("#txt-email")
+			.closest(".form-group")
+			.find(".help-block")
+			.html("Email inválido");
+		$("#txt-email")
+			.closest(".form-group")
+			.addClass("text-danger")
+			.removeClass("text-success");
+		return false;
+	} else {
+		$("#txt-email")
+			.closest(".form-group")
+			.find(".help-block")
+			.html("Email válido");
+		$("#txt-email")
+			.closest(".form-group")
+			.removeClass("text-danger")
+			.addClass("text-success");
+		$("#txt-email")
+			.closest(".form-group")
+			.find(".help-block")
+			.removeClass("text-danger");
+		return true;
+	}
+}
+
+function pagar(iTipoPago) {
+	$(".div-pago_curso").show();
+	$("#email").focus();
+	if (iTipoPago == 1) {
+		$("#btn-crear_cuenta").data("tipo_token", "formToken");
+		$("#btn-crear_cuenta").attr("data-tipo_token", "formToken");
+	} else if (iTipoPago == 2) {
+		$("#btn-crear_cuenta").data("tipo_token", "formTokenv2");
+		$("#btn-crear_cuenta").attr("data-tipo_token", "formTokenv2");
+	} else if (iTipoPago == 3) {
+		$("#btn-crear_cuenta").data("tipo_token", "formTokenv3");
+		$("#btn-crear_cuenta").attr("data-tipo_token", "formTokenv3");
+	}
+}
+const scrollToId = (id) => {
+	const element = document.getElementById(id);
+	element.scrollIntoView({ behavior: "smooth" });
+}
